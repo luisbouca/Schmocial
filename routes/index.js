e35903142cd09496e977bb59f0e5cac80636aac7
luis bouca <luismrbouca@gmail.com>
2018-12-10 15:22:00 +0000

    Home page design v2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* Home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Welcome to Schmocial' });
 });
+
+/* Login page.*/
+router.get('/signin', function(req, res, next) {
+  res.render('login', { title: 'Welcome to Schmocial' });
+});
+
+/*  Login */
+router.post('/signin', function(req, res, next) {
+  res.render('index', { title: 'Welcome to Schmocial' });
+});
+
+/* Sign up on Schmocial. */
+router.post('/signup', function(req, res, next) {
+  res.render('index', { title: 'Welcome to Schmocial' });
+});
+
+
 
 module.exports = router;
