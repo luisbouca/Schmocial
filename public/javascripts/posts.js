6d3cf8232a3b2ab4d0c7c7b7605070505c2e8cff
hugoGoncals <hugo._filipe_goncalves@outlook.pt>
2018-12-22 16:27:47 +0000

    vote system done and some layout improvments

--- public/javascripts/posts.js
+++ public/javascripts/posts.js
@@ -1,18 +1,41 @@
-var hashs = new Array()
-function guardaHashtags(teste) {
-    hashs.push(teste)
+var hashs = new Array()//array onde irão estar as hashtags
+function filterHash(teste) {
+    var url
+    var tipo
+    var dados
+    if($('#'+''+teste+'').attr('name')=='stand-by'){
+        hashs.push(teste)
+        $('#'+''+teste+'').attr('name', 'checked'); 
+        $('#'+''+teste+'').css("background-color", "red");
+    }else if($('#'+''+teste+'').attr('name')=='checked'){ 
+        for (var i=hashs.length-1; i>=0; i--) {
+            if (hashs[i] === teste) {
+                hashs.splice(i, 1); 
+                $('#'+''+teste+'').attr('name', 'stand-by'); 
+                $('#'+''+teste+'').css("background-color", "");
+            }}
+    }
+    if(hashs.length>0){
+        url = "http://localhost:3000/api/posts/filter/"
+        tipo = "POST"
+        dados=JSON.stringify({ hashtags: hashs })
+    }else{
+        url = "http://localhost:3000/api/posts/"
+        tipo = "GET"
+        dados=""
+    }
     $.ajax({
-        url: "http://localhost:3000/api/posts/filter/",
+        url: url,
         contentType: "application/json",
-        type: "POST",
-        data: JSON.stringify({ hashtags: hashs }),
+        type: tipo,
+        data: dados,
         dataType: "json",
         success: function (data) {
             $('#conteudoPosts').html('')
             for (i = 0; i < data.length; i++) {
                 var myvar = ""
                 var pic
-                var comments = ""
+                var comments = "" 
                 if (data[i].state == "public") {
                     if (data[i].picture) {
                         pic = '<img class="w3-margin-bottom" src=' + 'images/' + data[i].picture + ' style="width:100%;" />'
@@ -25,12 +48,13 @@ function guardaHashtags(teste) {
                         }
                     } else {
                         comments = ""
-                    }
+                    } 
                     myvar = myvar + '<div class="w3-container w3-card w3-black w3-round w3-margin"><br/>' +
                         '<img class="w3-left w3-circle w3-margin-right" style="width:60px;" src="/w3images/avatar6.png" alt="Avatar" /><span class="w3-right w3-opacity">32 min</span>' +
                         '    <h4>Angie Jane</h4><br/>' +
                         '    <hr class="w3-clear" />' +
-                        '    <p>' + data[i].title + '</p>' + pic + '<p>' + data[i].content + '</p><button class="w3-button w3-margin-bottom" type="button"><i class="fa fa-chevron-up"> Upvote</i></button><button class="w3-button w3-margin-bottom" type="button"><i class="fa fa-chevron-down"> Downvote</i></button><button class="w3-button w3-margin-bottom"' +
+                        '    <p>' + data[i].title + '</p>' + pic + '<p>' + data[i].content + '</p><div class="w3-card w3-black" style="display: inline;">'+
+                        '    <p class="w3-margin-bottom" id=' + data[i]._id + ' style="display: inline;">'+data[i].votes.length+' </p><i class="far fa-thumbs-up" style="font-size:24px; padding: 5px; display: inline;"></i></div><button class="w3-button w3-margin-bottom" type="button" id="teste" onclick="voteFunc('+currentUser+', '+data[i]._id+')"><i class="fa fa-chevron-up"> Upvote</i></button><button class="w3-button w3-margin-bottom" type="button"><i class="fa fa-chevron-down"> Downvote</i></button><button class="w3-button w3-margin-bottom"' +
                         '        type="button"><i class="fa fa-comment"> Comment </i></button>' +
                         '    <div class="w3-container w3-card commentSection" name=' + data[i]._id + '>' +
                         comments + '</div>'+
@@ -44,15 +68,22 @@ function guardaHashtags(teste) {
     });
 }
 
-function voteFunc(user,post){ 
+var currentUser
+function getUser(user) {
+    currentUser=user
+  }
+function voteFunc(user,post){  
+        var result = $('#'+''+post+'').text()
         $.ajax({
             type: "POST",
             contentType: "application/json",
             url: "http://localhost:3000/posts/vote",
             data: JSON.stringify({ user: user, post: post }),
-            dataType: "json"
-        })
-    
+            dataType: "json",
+            success: function () {
+            }
+        }) 
+        $('#'+''+post+'').text((parseInt(result)+1)) 
 }
 
 $(() => {
@@ -70,8 +101,7 @@ $(() => {
         var targetInput = $(e.target);
         if (targetInput.is('textarea')) {
             //alert('Typed while not focused on #myInput!');
-            if (e.which == 13) {
-                alert(targetInput.attr('name'));
+            if (e.which == 13) { 
                 e.preventDefault();
                 $('.commentSection').each(function () {
                     if ($(this).attr("name") == targetInput.attr('name')) {
